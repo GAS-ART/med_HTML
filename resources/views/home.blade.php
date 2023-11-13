257703e8298fb0d89b9b0938e8c3dc30b2f0aab3
GAS-ART <temoha1386@gmail.com>
2023-11-13 18:26:55 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+{{-- @section('title', __('titles.home.title'))
+@section('description', __('titles.home.description'))
+@section('ogTitle' ,__('titles.home.ogTitle'))
+@section('ogDescription', __('titles.home.ogDescription'))
+@section('ogUrl', __('titles.home.ogUrl'))
+@section('mainPopupPayload', 'Главная страница (' . url()->current() . ')') --}}
+
+@section('content')
+
+HERE IS HOME PAGE
+
+@endsection
